afe7e6abcac5718b16a849315195ccb74f4ad9f8
Murilo de Paula <murilosp.net@gmail.com>
2020-12-15 22:11:25 -0300

    Refatorando para versao no do JS

--- challenge-01/README.js
+++ challenge-01/README.js
@@ -1,53 +1,48 @@
-# Desafio Semana #1
+//# Desafio Semana #1
 
-```js
+
 // Declarar uma variável chamada `myvar`, sem valor.
-var myvar;
+let myvar;
 
 // Após declarada, atribua o valor 10 à variável `myvar`.
 myvar = 10;
 
 // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
-var soma = 15 + 8;
+let soma = 15 + 8;
 
 // Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
 soma++
 
 // Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
-soma *= 3;
+soma *= 3
 
 // Qual é o valor da variável `soma` até aqui?
-soma === 72;
+soma === 72
 
 // Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
-var souninja;
+const souninja =true
 
 // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
-var comida = ['arroz', 'feijão', 'ovo'];
+const comida = ['arroz', 'feijão', 'ovo']
 
 // Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
-comida[1];
+comida[1]
 
 // Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
-if(soma === myvar) {
-    console.log('soma é igual a myvar');
-} else {
-    console.log('soma é diferente de myvar');
-}
+if(soma === myvar)
+    console.log('soma é igual a myvar')
+else
+    console.log('soma é diferente de myvar')
+
 
 // Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
-if(myvar < soma) {
-    console.log('myvar é menor que soma');
-} else {
-    console.log('myvar não é menor que soma');
-}
+if(myvar < soma) 
+    console.log('myvar é menor que soma')
+ else 
+    console.log('myvar não é menor que soma')
 
 // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
-function divisao(numero1, numero2) {
-    return numero1 / numero2;
-}
+const divisao = (numero1, numero2) =>  numero1 / numero2
 
 // Invoque a função criada acima, passando os parâmetros 10 e 2.
- divisao(10,2);
-
-```
+ console.log(`resultado da divisão de 10 por 2 é ${divisao(10,2)}`)
